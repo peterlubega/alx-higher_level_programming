8ff1a0deb6406d45df8d62209e33ee5d0923f988
peterlubega <*peterlubega100@gmail.com>
2024-02-13 06:25:13 -0800

    feat: script that lists all the tables of a database in your MySQL server

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -1,3 +1,4 @@
---script that lists all the tables of a database in your MySQL server
+-- List all tables on a database
+-- execute: cat 3-list_tables.sql | mysql -hlocalhost -uroot -p mysql
 SHOW TABLES;
 
