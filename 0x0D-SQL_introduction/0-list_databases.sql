0fa751d894df025f848dc01dc69bf9e003215ce6
peterlubega <*peterlubega100@gmail.com>
2024-02-13 05:26:46 -0800

    feat: a script that lists all databases of your MySQL server

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,11 +1,1 @@
-#!/bin/bash
-
-# MySQL credentials
-MYSQL_USER="root"
-MYSQL_PASSWORD="your_password"
-
-# Command to list databases
-MYSQL_COMMAND="mysql -u$MYSQL_USER -p$MYSQL_PASSWORD -e 'SHOW DATABASES;'"
-
-# Execute the command
-eval $MYSQL_COMMAND
+SHOW DATABASES;
