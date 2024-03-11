16b262983d15113014ce34c7b48b26f0f25f41f2
peterlubega <peterlubega100@gmail.com>
2024-03-11 12:43:36 -0700

    feat: fixing 4,5,6 and 7

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Use strict mode to enforce better coding practices
+'use strict';
+
+const args = process.argv;
+
+console.log(args[2] + ' is ' + args[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node;
+'use strict';
+
+const strings = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+
+for (let i = 0; i < strings.length; i++) {
+  console.log(strings[i]);
+}
