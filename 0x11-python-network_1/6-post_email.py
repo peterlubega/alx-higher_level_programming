29d05f4c864d44ecb51c31f353ba5b0f82d211a8
peterlubega <peterlubega100@gmail.com>
2024-03-28 02:58:42 -0700

    feat: task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Script that takes in a URL and an email address, sends a POST request to the
+passed URL with the email as a parameter, and finally displays the body
+of the response.
+
+Usage: ./6-post_email.py <URL> <email>
+  - Displays the body of the response.
+"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    url = argv[1]
+    email = argv[2]
+    data = {"email": email}
+    req = requests.post(url, data=data)
+
+    print(req.text)
